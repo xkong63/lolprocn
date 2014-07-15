a355d385acdaf9863cc750fb5959626076c2ce13
alan <alan@Lenovo-PC>
2014-07-14 19:02:32 -0500

    Mastery, Rune DTO added

---
+++ src/java/com/lolprocn/entity/RunePageDto.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.lolprocn.entity;
+
+import java.util.Set;
+
+/**
+ *
+ * @author alan
+ */
+public class RunePageDto {
+    boolean current;
+    long id;
+    String name;
+    Set<RuneSlotDto> set;
+
+    public boolean isCurrent() {
+        return current;
+    }
+
+    public void setCurrent(boolean current) {
+        this.current = current;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<RuneSlotDto> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<RuneSlotDto> set) {
+        this.set = set;
+    }
+    
+}
